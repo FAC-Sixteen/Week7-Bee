be97d1ea2cda11b34ea4faca9e085fecb2c2c852
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-04-18 16:25:15 +0100

    getUser done with testing

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,20 +1,32 @@
 const dbConnection = require("../databases/db_connection.js");
 
-const getUsername = (username) => {
+const getUsernameValid = username => {
   return new Promise((resolve, reject) => {
-    dbConnection.query("SELECT username FROM users WHERE username = $1", [username],
-  (err, res) => {
-    if (err) reject(err);
-    // console.log(res.rows);
-    if (res.rows.length > 0) {
-      resolve(false);
-    } else {
-      resolve(true);
-    }
-  })
-  })
+    dbConnection
+      .query("SELECT username FROM users WHERE username = $1", [username])
+      .then(res => {
+        if (res.rows.length > 0) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      })
+      .catch(err => reject(err));
+  });
+};
+
+const getUser = username => {
+  return new Promise((resolve, reject) => {
+    dbConnection
+      .query("SELECT * FROM users WHERE username = $1", [username])
+      .then(res => {
+        if (!res.rows[0]) reject("Incorrect Username");
+        resolve(res.rows[0]);
+      });
+  });
 };
 
 module.exports = {
-  getUsername
-}
+  getUsernameValid,
+  getUser
+};
--- src/scripts/hash.js
+++ src/scripts/hash.js
@@ -1,13 +1,13 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const hashedPassword = (password) => {
-    return new Promise ((resolve, reject) => {
-        bcrypt.genSalt(10)
-            .then(salt => bcrypt.hash(password, salt))
-            .then( hash => resolve(hash))
-            .catch(err => reject(err))
-    
-        })
+const hashedPassword = password => {
+  return new Promise((resolve, reject) => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => resolve(hash))
+      .catch(err => reject(err));
+  });
 };
 
 //callback version
@@ -21,14 +21,15 @@ const hashedPassword = (password) => {
 // }
 
 const comparePassword = (password, hashedPassword) => {
-    return new Promise ((resolve, reject) => {
-        bcrypt.compare(password, hashedPassword).then(res => resolve(res)).catch(err => reject(err))
-
-    })
+  return new Promise((resolve, reject) => {
+    bcrypt
+      .compare(password, hashedPassword)
+      .then(res => resolve(res))
+      .catch(err => reject(err));
+  });
 };
 
 module.exports = {
-    hashedPassword,
-    comparePassword
-}
-
+  hashedPassword,
+  comparePassword
+};
