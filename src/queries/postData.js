22149ef767dbe9bb74bcc73f9ed7c95b0cf126b2
Burhan <42354400+Baalwaan@users.noreply.github.com>
2019-04-18 17:12:36 +0100

    postUser() created and written using promises

--- src/queries/postData.js
+++ src/queries/postData.js
@@ -1,25 +1,36 @@
-const dbConnection = require("../dbConnection.js");
+const dbConnection = require("../databases/db_Connection.js");
 
 const postNewUser = (obj) => {
   return new Promise ((resolve, reject) => {
-  dbConnection.query("INSERT INTO users(first_name, last_name, username, email, password) VALUES ($1, $2, $3, $4, $5)",
-[
-  obj.first_name,
-  obj.last_name,
-  obj.username,
-  obj.email,
-  obj.password
-],
-(err, res) => {
-  if (err) {
-    return cb(err);
-  }
-  cb(null);
+  dbConnection.query("INSERT INTO users(first_name, last_name, username, email, password) VALUES ($1, $2, $3, $4, $5)", [
+    obj.first_name,
+    obj.last_name,
+    obj.username,
+    obj.email,
+    obj.password])
+    .then(res => {
+      console.log('New user added to database ')
+      resolve(true)
+    })
+    .catch(err => reject(err))
+  
+})
 }
-);
-  })
-}
+// const fakeInput = {
+//   first_name: "Burhanda",
+//   last_name: "Bobhanda",
+//   username: "mistapepper",
+//   email: "bobbysebolao@gmail.com",
+//   password: "qwerty101!Q",
+//   confirmed_password: "qwerty101!Q"
+// }
+
+// console.log('this is >>>>>>',postNewUser(fakeInput));
 
 module.exports = {
   postNewUser
 }
+
+
+
+
