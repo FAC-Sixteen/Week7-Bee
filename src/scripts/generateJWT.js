8da7a65c18e5eeda112827b12b5963e29587e051
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-04-19 12:33:34 +0100

    loginHandler done with tests as well as small changes to fix a load of things

--- src/scripts/generateJWT.js
+++ src/scripts/generateJWT.js
@@ -4,12 +4,16 @@ require("env2")("./config.env");
 
 const secret = process.env.SECRET;
 
-var generateToken = (obj) => {
+var generateToken = obj => {
   return new Promise((resolve, reject) => {
-  jwt.sign(obj, secret)
-    .then(token => resolve(token))
-});
-}:
+    jwt.sign(obj, secret, (err, token) => {
+      if (err) reject(err);
+      resolve(token);
+    });
+  });
+};
+
+module.exports = generateToken;
 
 // const sign = (obj, secret) => {
 //   return new Promise
--- src/tests/getData.test.js
+++ src/tests/getData.test.js
@@ -4,22 +4,22 @@ const getData = require("../queries/getData");
 
 test("Testing getData function getUsername with existing", t => {
   runDbBuild
-    .then(res => getData.getUsernameValid('hard_al'))
+    .then(res => getData.getUsernameValid("hard_al"))
     .then(res => {
-      t.equals(res, 'Username already Exists', "Should return false");
-      t.end();
-     })
-    .catch(err => {
-      t.equals(err, false, "Should return false");
+      t.fail("username should exist");
       t.end();
     })
-})
+    .catch(err => {
+      t.equals(err, "Username already Exists", "Should return error message");
+      t.end();
+    });
+});
 
 test("Testing getData function getUsername without existing", t => {
   runDbBuild
-    .then(res => getData.getUsernameValid('sdkljgsjdglskj'))
+    .then(res => getData.getUsernameValid("sdkljgsjdglskj"))
     .then(res => {
       t.equals(res, true, "Should return true");
       t.end();
-     })
-})
+    });
+});
